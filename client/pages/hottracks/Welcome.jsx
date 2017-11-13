7d41e552301cbe0764820b9b29547ee5b377fce6
adrien candiotti <adrien.candiotti@gmail.com>
2017-11-13 13:27:16 -0500

    add - donation element on the right side of the hot track page

--- client/pages/hottracks/Welcome.jsx
+++ client/pages/hottracks/Welcome.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Panel } from 'react-bootstrap'
+import { Panel, Col, Media } from 'react-bootstrap'
 
 import HotTracks from './HotTracks.jsx'
 
@@ -9,11 +9,34 @@ import HotTracks from './HotTracks.jsx'
 //        -> fix CSS
 export class Welcome extends Component {
   render () {
+    const panelTitle = (
+      <h3>Welcome to sound-nuggets! (the openwhyd frontend)</h3>
+    )
+
     return <div className='page-container container'>
-      <Panel id='welcome'>
-        <h3>Welcome to sound-nuggets, an openwhyd frontend</h3>
-        <HotTracks {...this.props} />
+      <Col md={7}>
+      <Panel id='welcome' header={panelTitle}>
+          <HotTracks {...this.props} />
       </Panel>
+      </Col>
+      <Col md={4}>
+        <Panel id='donate'>
+          <h4>💓 Openwhyd needs you!</h4>
+          <Media.Left>
+            <img src='/img/github-octocat.png'height={64} width={64}/>
+          </Media.Left>
+          <Media.Body>
+            <p>Help us keep Openwhyd free, independant and without ads.</p>
+            <p><b>Donate today!</b></p>
+            {[1, 2, 5, 10, 20].map((value) => {
+              className = value > 9 ? 'donate-big' : 'donate-small'
+              return <Col md={1} className={'donate ' + className}>
+              <a href='https://opencollective.com/openwhyd/donate'>{value}$</a>
+              </Col>
+            })}
+          </Media.Body>
+        </Panel>
+      </Col>
     </div>
   }
 }
