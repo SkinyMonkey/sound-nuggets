417f63be9cd4d2af75b33a59fa2ce40c91d1daa1
adrien candiotti <adrien.candiotti@gmail.com>
2017-11-14 22:59:44 -0500

    add - facebook login

--- client/actions/session.js
+++ client/actions/session.js
@@ -18,10 +18,30 @@ export const resume = () => {
   }
 }
 
-export const login = (email, password) => {
+export const emailLogin = (email, password) => {
   return (dispatch) => {
     return new Promise((resolve, reject) => {
       Meteor.call('openwhyd.login.email', email, password, (error, session) => {
+        if (session.error) return reject(session.error)
+
+				cookie.removeItem(WHYD_COOKIE)
+        session.cookie.split(';').forEach((cookie) => {
+          document.cookie = cookie
+        })
+
+				localStorage.setItem('session', JSON.stringify(session))
+        dispatch({ type: LOGIN, payload: session })
+
+        return resolve(session.currentUser._id)
+      })
+    })
+  }
+}
+
+export const facebookLogin = (facebookId, accessToken) => {
+  return (dispatch) => {
+    return new Promise((resolve, reject) => {
+      Meteor.call('openwhyd.login.facebook', facebookId, accessToken, (error, session) => {
         if (session.error) return reject(session.error)
 
 				cookie.removeItem(WHYD_COOKIE)
