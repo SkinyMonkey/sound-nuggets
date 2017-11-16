c1af15277a5718c018adedac505c62ea4eda843c
adrien candiotti <adrien.candiotti@gmail.com>
2017-11-16 11:38:41 -0500

    add - Donate component, added to Stream page, Stream page is encapsulated into a dummy container that could generalised

--- client/pages/profile/tracks/Stream.jsx
+++ client/pages/profile/tracks/Stream.jsx
@@ -1,10 +1,25 @@
+import React from 'react'
 import { connect } from 'react-redux'
+
+import { Col } from 'react-bootstrap'
 
 import TrackList from './TrackList.jsx'
 import Container from '../../../components/Container.jsx'
 import SearchWrapper from '../../../components/SearchWrapper.jsx'
+import Donate from '../../../components/Donate.jsx'
 
 import withMethodData from '../../../components/withMethodData.jsx'
+
+const DummyContainer = (wrappedComponent) => {
+  return (props) => <div className='page-container container'>
+    <Col md={7} >
+      <div id='page'>
+      {React.createElement(wrappedComponent, props)}
+      </div>
+    </Col>
+    <Donate/>
+  </div>
+}
 
 const tracksFromAPI = withMethodData((props, done) => {
   const profileId = props.match.params.profileId
@@ -24,4 +39,4 @@ function mapStateToProps (state, ownProps) {
   }
 }
 
-export default connect(mapStateToProps)(Stream)
+export default connect(mapStateToProps)(DummyContainer(Stream))
